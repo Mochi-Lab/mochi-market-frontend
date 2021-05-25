601958841e17a6061ef45859dfdf5d4cb62d8f80
Ngo Nolan <ngovannghia1997kma@gmail.com>
2021-05-25 17:01:23 +0700

    change: new-ui-create-collection

--- src/Views/Create/index.js
+++ src/Views/Create/index.js
@@ -12,17 +12,14 @@ export default function Create() {
   return (
     <div className='center create-pt'>
       <div className='create-box'>
-        <h2 className='textmode'>Create collectible</h2>
-        <p className='textmode'>
-          Choose “Single” if you want your collectible to be one of a kind or “Multiple” if you want
-          to sell one collectible multiple times
+        <h1 className='textmode title-create-collection'>Create collectible</h1>
+        <p className='textmode description-create-collection'>
+          Choose <span>“Single”</span> if you want your collectible to be one of a kind or{' '}
+          <span>“Multiple”</span> if you want to sell one collectible multiple times
         </p>
 
         <div className='justifyContentSa'>
-          <div
-            className='box input-mode-bc slt center mr-1d5rem'
-            onClick={() => push('/create/erc721/')}
-          >
+          <div className='box input-mode-bc slt center' onClick={() => push('/create/erc721/')}>
             <img src={create721Icon} alt='single' />
             <p>Single</p>
           </div>
@@ -32,10 +29,11 @@ export default function Create() {
             <p>Multiple</p>
           </div>
         </div>
-
-        <p className='textmode'>
-          We do not own your private keys and cannot access your funds without your confirmation
-        </p>
+        <div className='noti-create-collection'>
+          <p className='textmode '>
+            We do not own your private keys and cannot access your funds without your confirmation
+          </p>
+        </div>
       </div>
     </div>
   );
