a72b72a7c8372f606dc8cca42e2a25e5edc5c6dd
tranchien2002 <chie.tm2002@gmail.com>
2021-06-04 12:22:03 +0700

    redirect noti

--- src/Components/Notification/index.js
+++ src/Components/Notification/index.js
@@ -1,4 +1,4 @@
-import { notification } from 'antd';
+import { notification, Button } from 'antd';
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
@@ -19,10 +19,33 @@ export default function Notification() {
     });
   };
 
+  const openSuccessRedirect = (message, fn) => {
+    notification.success({
+      message: 'Message',
+      description: (
+        <div>
+          <p>{message}</p>
+          <Button onClick={fn}>Go to Inventory</Button>
+        </div>
+      ),
+    });
+  };
+
   useEffect(() => {
     const showNotification = (noti) => {
-      if (noti.type === 'error') openError(noti.message);
-      else openSuccess(noti.message);
+      switch (noti.type) {
+        case 'error':
+          openError(noti.message);
+          break;
+        case 'success':
+          openSuccess(noti.message);
+          break;
+        case 'redirect-profile':
+          openSuccessRedirect(noti.message, noti.fn);
+          break;
+        default:
+          console.log(noti.type);
+      }
     };
     if (!!noti) showNotification(noti);
   }, [noti]);
