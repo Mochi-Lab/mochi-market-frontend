a22f8e3f1af549d98d28805f8bbd5b2ce39621de
conglt10 <conglt1712@gmail.com>
2021-06-08 09:26:40 +0700

    polygon network

--- src/utils/getUrlsSubgraph.js
+++ src/utils/getUrlsSubgraph.js
@@ -1,13 +1,8 @@
 const urlsSubgraph = {
-  //BSC Mainnet
-  56: {
-    url1155: 'https://api.thegraph.com/subgraphs/name/tranchien2002/eip1155-bsc-main',
-    url721: 'https://api.thegraph.com/subgraphs/name/tranchien2002/eip721-bsc-main',
-  },
-  //BSC Testnet
-  97: {
-    url1155: 'https://api.thegraph.com/subgraphs/name/tranchien2002/eip1155-bsc',
-    url721: 'https://api.thegraph.com/subgraphs/name/tranchien2002/eip721-bsc',
+  //Polygon Mainnet
+  137: {
+    url1155: 'https://api.thegraph.com/subgraphs/name/tranchien2002/eip1155-matic',
+    url721: 'https://api.thegraph.com/subgraphs/name/tranchien2002/eip721-matic',
   },
 };
 
