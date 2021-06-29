d18c620f244f12e4a85c2827bdef0e1845713af5
duong <duong.pv@icloud.com>
2021-06-29 22:00:21 +0700

    add rounded corner for avatar-token & add more radius on Search Boxes

--- src/Components/BannerSearchHome/index.js
+++ src/Components/BannerSearchHome/index.js
@@ -5,6 +5,7 @@ import Slider from 'react-slick';
 import { setStrSearch } from 'store/actions';
 import { useDispatch, useSelector } from 'react-redux';
 import { useRef, useState } from 'react';
+import { Input } from 'antd';
 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -16,7 +17,8 @@ export default function BannerSearchHome({ carouselBanner, inputSearch }) {
   const { strSearch } = useSelector((state) => state);
 
   const browse = useRef(null);
-  const searchNFT = (text) => {
+  const searchNFT = (event) => {
+    const text = event.target.value;
     dispatch(setStrSearch(text));
   };
 
@@ -53,12 +55,6 @@ export default function BannerSearchHome({ carouselBanner, inputSearch }) {
           <div className={'hs' + (searchBoxFocused ? ' active' : '')}>
             <div
               className='search-icon center search-input'
-              style={{
-                height: '3rem',
-                width: '3rem',
-                borderRadius: '16px 0px 0px 16px',
-                background: '#ffffff',
-              }}
             >
               <SearchOutlined />
             </div>
@@ -67,7 +63,7 @@ export default function BannerSearchHome({ carouselBanner, inputSearch }) {
               className='search-input home-search'
               placeholder='Search'
               ref={inputSearch}
-              onChange={(e) => searchNFT(e.target.value)}
+              onChange={searchNFT}
               onKeyDown={handleKeyDown}
               value={strSearch}
               onFocus={handleOnFocus}
