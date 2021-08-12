1066fb349a938d9e96da7a96699ac553b4b78973
duong <duong.pv@icloud.com>
2021-08-12 20:12:29 +0700

    Reduce re-render on NFTCard

---
+++ src/Components/Common/NFTCard.js
@@ -0,0 +1,152 @@
+import React, { useEffect, useState } from "react";
+import { Card, Row, Col, Popover } from 'antd';
+import imgNotFound from 'Assets/notfound.png';
+import { getCollection } from 'store/actions';
+import store from 'store/index';
+import { getDetailNFT } from 'APIs/NFT/Get';
+import { useHistory } from 'react-router-dom';
+import { getSymbol } from 'utils/getContractAddress';
+import moment from 'moment';
+import tick from 'Assets/icons/tick-green.svg';
+import { handleChildClick, objToString } from 'utils/helper';
+import { isArray } from 'lodash';
+
+const __NFTCardLoader = () => {
+  return (
+    <Card
+      className='card-nft card-nft-content-loader'
+      cover={
+        <div className='wrap-cover'>
+          <div className='NFTResource-Wrapper'>
+            <img
+              className='display-resource-nft'
+              src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII='
+              alt=''
+            />
+          </div>
+        </div>
+      }
+    >
+      <Row justify='space-between'>
+        <Col className='footer-card-left'>
+          <div className='name-collection'>&nbsp;</div>
+          <div className='name-nft'>&nbsp;</div>
+        </Col>
+      </Row>
+    </Card>)
+}
+
+export const __NFTCardDetail = ({ chainId, token, detailNFT, collectionName, verifiedContracts }) => {
+  const history = useHistory()
+  const collectionUrl = `/collection/${chainId}/${token.collectionAddress}`
+  const itemUrl = `/token/${chainId}/${token.collectionAddress}/${token.tokenId}/${token.sellId}`
+  const onClick = (event) => {
+    event.preventDefault();
+    let eventTarget = event.target;
+    history.push(
+      eventTarget.matches("span.link-collection-name") ? collectionUrl : itemUrl
+    )
+  }
+  return (
+    <a href={itemUrl} onClick={onClick}>
+      <Card
+        hoverable
+        cover={
+          <div className='wrap-cover'>
+            <div
+              className='blurred-background'
+              style={{
+                backgroundImage: `url(${token.image || detailNFT.image})`,
+              }}
+            />
+            <div className='NFTResource-Wrapper'>
+              <img
+                alt={`img-nft-${token.tokenId}`}
+                src={token.image || detailNFT.image}
+                className='display-resource-nft'
+              />
+            </div>
+          </div>
+        }
+        className='card-nft'
+      >
+        {!!token.attributes && token.attributes.length > 0 && (
+          <Popover
+            onClick={handleChildClick}
+            placement='bottomLeft'
+            content={token.attributes.map((attr, i) => (
+              <div key={i} onClick={handleChildClick}>
+                <strong>{attr.trait_type}</strong>:{' '}
+                {isArray(attr.value)
+                  ? attr.value.join(', ')
+                  : !!attr.display_type &&
+                    attr.display_type.toLowerCase() === 'date' &&
+                    !!moment(attr.value).isValid()
+                    ? moment(
+                      attr.value.toString().length < 13 ? attr.value * 1000 : attr.value
+                    ).format('DD-MM-YYYY')
+                    : typeof attr.value === 'object'
+                      ? objToString(attr.value)
+                      : attr.value}
+              </div>
+            ))}
+          >
+            <div className='attribs-nft' onClick={handleChildClick}>
+              Stats
+            </div>
+          </Popover>
+        )}
+        {!!token.price && (
+          <div className='price-nft textmode'>
+            <span>{token.price}</span> <b>{getSymbol(chainId)[token.token]}</b>
+          </div>
+        )}
+        <Row justify='space-between'>
+          <Col className='footer-card-left'>
+            <div className='name-collection'>
+              <span
+                className='link-collection-name'
+                tag='span'
+              >
+                {collectionName || token.nameCollection}
+              </span>
+              {
+                verifiedContracts.includes(token.collectionAddress.toLocaleLowerCase()) && (
+                  <img src={tick} alt='icon-tick' className='icon-tick' />
+                )
+              }
+            </div>
+            <div className='name-nft textmode'>
+              {token.name || detailNFT.name}
+            </div>
+          </Col>
+        </Row>
+      </Card>
+    </a>
+  )
+}
+
+export const useDetailNFT = (chainId, token,) => {
+  const [detailNFT, setDetailNFT] = useState(null);
+  useEffect(() => {
+    async function fetchDetail() {
+      if (!token) return setDetailNFT({ name: '', description: '', image: imgNotFound });
+      try {
+        let nft = await getDetailNFT(chainId, token.collectionAddress, token.tokenId);
+        if (!nft.name || nft.name === 'Unnamed') nft.name = 'ID: ' + token.tokenId;
+        token.nameCollection = (
+          await store.dispatch(getCollection(token.collectionAddress, null))
+        ).collection.name;
+        setDetailNFT(nft);
+      }
+      catch (error) {
+        setDetailNFT({ name: 'Unnamed', description: '', image: imgNotFound });
+      }
+    }
+    fetchDetail();
+  }, [chainId, token]);
+  return detailNFT;
+}
+
+export const NFTCardLoader = React.memo(__NFTCardLoader)
+export const NFTCardDetail = React.memo(__NFTCardDetail)
--- src/Components/NFTsCardBrowse/index.js
+++ src/Components/NFTsCardBrowse/index.js
@@ -1,142 +1,17 @@
-import { Card, Row, Col, Popover } from 'antd';
-import { useState, useEffect, useCallback } from 'react';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { getSymbol } from 'utils/getContractAddress';
-import imgNotFound from 'Assets/notfound.png';
-import { getCollection } from 'store/actions';
+import { Row, Col } from 'antd';
+import React, { useCallback } from 'react';
 import store from 'store/index';
-import moment from 'moment';
-import tick from 'Assets/icons/tick-green.svg';
 import './index.scss';
 import 'Assets/css/common-card-nft.scss';
-import { handleChildClick, objToString } from 'utils/helper';
 import { BottomScrollListener } from 'react-bottom-scroll-listener';
 import LoadingScroll from 'Components/LoadingScroll';
 import { useStateWithCallbackLazy } from 'use-state-with-callback';
-import { getDetailNFT } from 'APIs/NFT/Get';
-import { isArray } from 'lodash';
+import { NFTCardLoader, NFTCardDetail, useDetailNFT } from 'Components/Common/NFTCard'
 
-function NFTsCard({ token, collectionName }) {
-  const { chainId, verifiedContracts } = useSelector((state) => state);
-  const [detailNFT, setDetailNFT] = useState(null);
-  useEffect(() => {
-    async function fetchDetail() {
-      if (!!token) {
-        try {
-          let nft = await getDetailNFT(chainId, token.collectionAddress, token.tokenId);
-          if (!nft.name || nft.name === 'Unnamed') nft.name = 'ID: ' + token.tokenId;
-          token.nameCollection = (
-            await store.dispatch(getCollection(token.collectionAddress, null))
-          ).collection.name;
-          setDetailNFT(nft);
-        } catch (error) {
-          setDetailNFT({ name: 'Unnamed', description: '', image: imgNotFound });
-        }
-      } else {
-        setDetailNFT({ name: '', description: '', image: imgNotFound });
-      }
-    }
-    if (!detailNFT) {
-      fetchDetail();
-    }
-  }, [token, chainId, detailNFT]);
-
-  const visible = !!detailNFT && !!detailNFT.name && !!token;
-
-  return detailNFT !== null ? (
-    <>
-      {!visible ? null : (
-        <Col
-          className='gutter-row'
-          xs={{ span: 24 }}
-          sm={{ span: 12 }}
-          md={{ span: 8 }}
-          lg={{ span: 8 }}
-          xl={{ span: 6 }}
-          xxl={{ span: 6 }}
-        >
-          <Link
-            to={`/token/${chainId}/${token.collectionAddress}/${token.tokenId}/${token.sellId}`}
-            target='_blank'
-          >
-            <Card
-              hoverable
-              cover={
-                <div className='wrap-cover'>
-                  <div
-                    className='blurred-background'
-                    style={{
-                      backgroundImage: `url(${!!token.image ? token.image : detailNFT.image})`,
-                    }}
-                  />
-                  <div className='NFTResource-Wrapper'>
-                    <img
-                      alt={`img-nft-${token.tokenId}`}
-                      src={!!token.image ? token.image : detailNFT.image}
-                      className='display-resource-nft'
-                    />
-                  </div>
-                </div>
-              }
-              className='card-nft'
-            >
-              {!!token.attributes && token.attributes.length > 0 && (
-                <Popover
-                  onClick={handleChildClick}
-                  placement='bottomLeft'
-                  content={token.attributes.map((attr, i) => (
-                    <div key={i} onClick={handleChildClick}>
-                      <strong>{attr.trait_type}</strong>:{' '}
-                      {isArray(attr.value)
-                        ? attr.value.join(', ')
-                        : !!attr.display_type &&
-                          attr.display_type.toLowerCase() === 'date' &&
-                          !!moment(attr.value).isValid()
-                        ? moment(
-                            attr.value.toString().length < 13 ? attr.value * 1000 : attr.value
-                          ).format('DD-MM-YYYY')
-                        : typeof attr.value === 'object'
-                        ? objToString(attr.value)
-                        : attr.value}
-                    </div>
-                  ))}
-                >
-                  <div className='attribs-nft' onClick={handleChildClick}>
-                    Stats
-                  </div>
-                </Popover>
-              )}
-              {!!token.price && (
-                <div className='price-nft textmode'>
-                  <span>{token.price}</span> <b>{getSymbol(chainId)[token.token]}</b>
-                </div>
-              )}
-              <Row justify='space-between'>
-                <Col className='footer-card-left'>
-                  <div className='name-collection'>
-                    <Link
-                      to={`/collection/${chainId}/${token.collectionAddress}`}
-                      className='link-collection-name'
-                      tag='span'
-                    >
-                      {!!collectionName ? collectionName : token.nameCollection}
-                    </Link>
-                    {verifiedContracts.includes(token.collectionAddress.toLocaleLowerCase()) && (
-                      <img src={tick} alt='icon-tick' className='icon-tick' />
-                    )}{' '}
-                  </div>
-                  <div className='name-nft textmode'>
-                    {!!token.name ? token.name : detailNFT.name}
-                  </div>
-                </Col>
-              </Row>
-            </Card>
-          </Link>
-        </Col>
-      )}
-    </>
-  ) : (
+const NFTsCard = React.memo(({ token, collectionName }) => {
+  const { chainId, verifiedContracts } = store.getState()
+  const detailNFT = useDetailNFT(chainId, token);
+  return (
     <Col
       className='gutter-row'
       xs={{ span: 24 }}
@@ -146,34 +21,17 @@ function NFTsCard({ token, collectionName }) {
       xl={{ span: 6 }}
       xxl={{ span: 6 }}
     >
-      <Card
-        className='card-nft card-nft-content-loader'
-        cover={
-          <div className='wrap-cover'>
-            <div className='NFTResource-Wrapper'>
-              <img
-                className='display-resource-nft'
-                src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII='
-                alt=''
-              />
-            </div>
-          </div>
-        }
-      >
-        <Row justify='space-between'>
-          <Col className='footer-card-left'>
-            <div className='name-collection'>&nbsp;</div>
-            <div className='name-nft'>&nbsp;</div>
-          </Col>
-        </Row>
-      </Card>
+      {
+        detailNFT === null
+          ? <NFTCardLoader />
+          : <NFTCardDetail {...{ chainId, token, detailNFT, collectionName, verifiedContracts }} />
+      }
     </Col>
   );
-}
+})
 
 export default function NFTsCardBrowse({
   tokens,
-  strSearchInCollection,
   fetchExplore,
   isEndOfOrderList,
   loadingScroll,
@@ -202,7 +60,6 @@ export default function NFTsCardBrowse({
               <NFTsCard
                 key={token.sellId}
                 token={token}
-                strSearch={strSearchInCollection}
                 collectionName={collectionName}
               />
             ))}
