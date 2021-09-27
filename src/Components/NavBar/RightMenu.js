05b5e0f829cd6f25b8e9a3d945ab8a7fae92d2f6
duong <duong.pv@icloud.com>
2021-09-27 10:47:21 +0700

    add NFT bridge link

--- src/Components/NavBar/RightMenu.js
+++ src/Components/NavBar/RightMenu.js
@@ -56,6 +56,16 @@ const RightMenu = ({ onClose }) => {
     <Menu selectable={false} mode={screen.md && screen.lg ? 'horizontal' : 'inline'}>
       {generateMenuItemForRouteKey('/browse')}
       {generateMenuItemForRouteKey('/submit-Nfts')}
+      <Menu.Item key='bridge'>
+        <a
+          href='https://nftbridge.mochi.market/'
+          target='_blank'
+          rel='noreferrer'
+          className='text-white'
+        >
+          <div className='menu-button'>NFT Bridge</div>
+        </a>
+      </Menu.Item>
       {!!walletAddress && (
         generateMenuItemForRouteKey('/profile')
       )}
