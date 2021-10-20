0ebbbcbd02123c1959f0c23736d04995894e843a
tranchien2002 <chie.tm2002@gmail.com>
2021-10-20 16:50:28 +0700

    fix chainid str

--- src/Views/Profile/TabOwner.js
+++ src/Views/Profile/TabOwner.js
@@ -4,50 +4,41 @@ import NFTsProfile from 'Components/NFTsProfile';
 import { getNFTsOfOwner } from 'store/actions';
 import { getDetailNFT, getListNFTsOwner } from 'APIs/NFT/Get';
 import store from 'store/index';
+import { noop } from 'lodash';
 
 export default function TabOwner({ address }) {
-  const { chainId } = useSelector((state) => state);
+  let { chainId } = useSelector((state) => state);
 
   const [loadingGetOwner, setloadingGetOwner] = useState(false);
   const [loadingScroll, setLoadingScroll] = useState(false);
   const [nftsOwner, setNftsOwner] = useState();
   const [skip, setSkip] = useState(0);
   const [isEndOfOrderList, setIsEndOfOrderList] = useState(false);
+  chainId = +chainId;
   const fetchOwner = useCallback(async () => {
-    if (!!chainId) {
-      try {
-        if (skip > 1) {
-          setLoadingScroll(true);
-        }
-        let listRaw721 = await getListNFTsOwner(address, skip, 20);
-        let exp = await Promise.all(
-          listRaw721.map(async (e) => {
-            let nft = await getDetailNFT(chainId, e.Address, e.TokenID);
-            if (!nft.name || nft.name === 'Unnamed') nft.name = 'ID: ' + e.TokenID;
-            nft['is1155'] = false;
-            return nft;
-          })
-        );
-        // console.log(nftsOwner, exp, 'ownmer');
-        setSkip(skip + 20);
-        setNftsOwner((nftsOwner) => (!!nftsOwner ? [...nftsOwner, ...exp] : [...exp]));
-        if (exp.length < 20) setIsEndOfOrderList(true);
-        setLoadingScroll(false);
-      } catch (error) {
-        console.log({ error });
+    console.log(`address, skip, chainId`, address, skip, chainId);
+    if (!chainId || isEndOfOrderList) return;
+    try {
+      if (skip > 1) {
+        setLoadingScroll(true);
       }
+      let listRaw721 = await getListNFTsOwner(address, skip, 20);
+      let exp = await Promise.all(
+        listRaw721.map(async (e) => {
+          let nft = await getDetailNFT(chainId, e.Address, e.TokenID);
+          if (!nft.name || nft.name === 'Unnamed') nft.name = 'ID: ' + e.TokenID;
+          nft['is1155'] = false;
+          return nft;
+        })
+      );
+      setNftsOwner((nftsOwner) => (!!nftsOwner ? [...nftsOwner, ...exp] : [...exp]));
+      setSkip((c) => c + 20);
+      if (exp.length < 20) setIsEndOfOrderList(true);
+      setLoadingScroll(false);
+    } catch (error) {
+      console.log({ error });
     }
-
-    //   setloadingGetOwner(true);
-    //   await store.dispatch(getNFTsOfOwner(address));
-    //   setloadingGetOwner(false);
-  }, [address, skip, chainId]);
-
-  // useEffect(() => {
-  //   if (!!chainId && !!address) {
-  //     fetchOwner();
-  //   }
-  // }, [fetchOwner, address, chainId, walletAddress]);
+  }, [address, skip, chainId, isEndOfOrderList]);
 
   useEffect(() => {
     async function loadingInit() {
@@ -55,7 +46,6 @@ export default function TabOwner({ address }) {
       await fetchOwner();
       setloadingGetOwner(false);
     }
-    console.log(nftsOwner, chainId, address);
     if (!nftsOwner) {
       loadingInit();
     }
